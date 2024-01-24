697ef5bc6049e7e619e9a3cfaa57493683bfe426
Mateusz Sawicki <mathewsawicki@gmail.com>
2024-01-24 15:19:15 +0100

    add postgres options

--- Confab.Shared.Infrastructure/Extensions.cs
+++ Confab.Shared.Infrastructure/Extensions.cs
@@ -4,6 +4,7 @@ using Confab.Shared.Infrastructure.Exceptions;
 using Confab.Shared.Infrastructure.Time;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Runtime.CompilerServices;
 
@@ -36,6 +37,20 @@ namespace Confab.Shared.Infrastructure
             });
 
             return app;
+        }
+
+        public static T GetOptions<T>(this IServiceCollection services, string sectionName) where T : new()
+        {
+            using var serviceProvider = services.BuildServiceProvider();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            return configuration.GetOptions<T>(sectionName);
+        }
+
+        public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : new()
+        {
+            var options = new T();
+            configuration.GetSection(sectionName).Bind(options);
+            return options;
         }
     }
 }
---
+++ Confab.Shared.Infrastructure/Postgres/PostgresOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Confab.Shared.Infrastructure.Postgres
+{
+    public class PostgresOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}
